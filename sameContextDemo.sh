a75671e7291d49010e1b60817bda5fea0034bbe8
Cem Gunduz <cem@BB-SYSTEM-0396.local>
2018-02-07 16:22:26 +0100

    Clean up

--- sameContextDemo.sh
+++
@@ -1,5 +0,0 @@
-mvn spring-boot:run -f registry/pom.xml &
-sleep 2
-mvn spring-boot:run -f serviceB/pom.xml &
-mvn spring-boot:run -f serviceA/pom.xml -Dserver.servlet-path=/ -Dserver.port=8083 &
-mvn spring-boot:run -f serviceA/pom.xml -Dserver.servlet-path=/ -Dserver.port=8084 &
